f1647138a8471ea8ff967221a2b1718db81f73d1
RicardoBarrote <ricardoambscfc@gmail.com>
2025-04-17 21:17:33 -0300

    Utilizando do tipo RECORD, para criação do UserRequestDto

---
+++ src/main/java/method/factory/controller/UserRequestDto.java
@@ -0,0 +1,16 @@
+package method.factory.controller;
+
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import method.factory.infrastructure.enums.UserTypeEnum;
+
+public record UserRequestDto(@NotNull
+                             @NotBlank
+                             String name,
+                             @NotNull
+                             @NotBlank
+                             String cpfOrCnpj,
+                             @Enumerated
+                             UserTypeEnum type) {
+}
